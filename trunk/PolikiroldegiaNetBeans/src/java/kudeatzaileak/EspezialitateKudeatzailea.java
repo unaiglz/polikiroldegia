6526cf5028399634b6e4e838ebfa0f3316af1f90
iratxe46@hotmail.com <iratxe46@hotmail.com@3cf74486-e848-c92b-7ab3-dbaed62a6763>
2012-05-11 10:42:06 +0000

    Monitore eta espezialitate taulek funtzionatzen dute. Bezero taulak ez.

--- trunk/PolikiroldegiaNetBeans/src/java/kudeatzaileak/EspezialitateKudeatzailea.java
+++ trunk/PolikiroldegiaNetBeans/src/java/kudeatzaileak/EspezialitateKudeatzailea.java
@@ -1,25 +1,22 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package kudeatzaileak;
 
 import hibernate.HibernateKud;
 import java.util.ArrayList;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import pojo.Espezialitatea;
 
 
-@ManagedBean
-@RequestScoped
 public class EspezialitateKudeatzailea {
 
-    /**
-     * Creates a new instance of TarifaKudeatzailea
-     */
-    public EspezialitateKudeatzailea() {
+    private static EspezialitateKudeatzailea instantzia = new EspezialitateKudeatzailea();
+   
+    private EspezialitateKudeatzailea() {
     }
+
+    public static EspezialitateKudeatzailea getInstantzia() {
+        return instantzia;
+    }
+    
+   
 
     public ArrayList<Espezialitatea> EspezialitateakLortu() {
         return (ArrayList<Espezialitatea>) HibernateKud.getInstance().execHQL("from Espezialitatea");
@@ -27,7 +24,7 @@ public class EspezialitateKudeatzailea {
     
     public void espezialitateaEzabatu(Espezialitatea esp){
         
-        HibernateKud.getInstance().ezabatu2(esp);
+        HibernateKud.getInstance().ezabatu(esp);
     }
     
     public void espezialitateaGorde(Espezialitatea esp){
---
+++ trunk/PolikiroldegiaNetBeans/src/java/zerrendak/MonitoreZerrenda.java
@@ -0,0 +1,63 @@
+package zerrendak;
+
+import java.util.ArrayList;
+import kudeatzaileak.MonitoreKudeatzailea;
+import pojo.Monitorea;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class MonitoreZerrenda {
+    ArrayList<Monitorea> zerrenda = new ArrayList<Monitorea>(); 
+    private Monitorea selectedMonitorea; 
+    
+    private Monitorea moni;     
+  
+    
+    public MonitoreZerrenda() {
+        moni = new Monitorea();
+        zerrenda = MonitoreKudeatzailea.getInstantzia().monitoreakLortu();
+    }
+    
+
+    public ArrayList<Monitorea> getZerrenda() {
+        return zerrenda;
+    }
+
+    public void setZerrenda(ArrayList<Monitorea> zerrenda) {
+        this.zerrenda = zerrenda;
+    }
+  
+    public Monitorea getSelectedMonitorea() {  
+        return selectedMonitorea;  
+    }  
+    public void setSelectedMonitorea(Monitorea selectedMonitorea) {  
+        this.selectedMonitorea = selectedMonitorea;  
+    }
+
+    public Monitorea getMoni() {
+        return moni;
+    }
+
+    public void setMoni(Monitorea moni) {
+        this.moni = moni;
+    }
+    
+    
+    
+    /*Ez dabil*/
+    public void ezabatuMonitorea() { 
+        
+        zerrenda.remove(selectedMonitorea); 
+        MonitoreKudeatzailea.getInstantzia().espezialitateaEzabatu(selectedMonitorea);
+    }
+   
+    
+    public void monitoreaGorde() {
+        MonitoreKudeatzailea.getInstantzia().monitoreaGorde(new Monitorea(moni.getId(),moni.getIzena(),moni.getAbizena()));
+        moni = new Monitorea();
+         
+    } 
+    
+}
